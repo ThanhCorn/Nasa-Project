8053cac88cfb743b5ae69dfdc3a749beceff2a90
ThanhCorn <nnnt10122000@gmail.com>
2023-07-07 14:16:24 +0700

    last

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+
+const port = process.env.PORT || 8000;
+const http = require('http');
+const app = require('./app');
+require('dotenv').config();
+
+
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
+const { mongoConnect } = require('./services/mongo');
+
+const server = http.createServer(app);
+
+
+async function startServer() {
+    await mongoConnect();
+    await loadPlanetsData();
+    await loadLaunchData();
+
+    server.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    })
+}
+startServer();
